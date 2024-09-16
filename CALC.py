4d611e36743fe6bc0e9d4fc3dd681884b7622e4c
kaushick <82633569+kaushick2000@users.noreply.github.com>
2024-09-16 15:22:28 -0500

    Update CALC.py with square root code

--- CALC.py
+++ CALC.py
@@ -10,6 +10,9 @@ def multiply(x, y):
 
 def divide(x, y):
     return x / y
+    
+def sqrt(x):
+    return x**0.5
 
 
 print("Select operation.")
@@ -17,12 +20,13 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Square root")
 
 while True:
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4','5'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -41,6 +45,9 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+            
+        elif choice=='5':
+            print("Sqrt=",sqrt(num1))
         
 
         next_calculation = input("Let's do next calculation? (yes/no): ")
